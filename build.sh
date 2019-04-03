284fd0c255be18777b7d7d9e42d15c8f6bbdae97
Louie Simpson <louie@louie.io>
2019-04-02 22:51:56 -0700

    Fixed styling on the search input box

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-echo "python ./build_index.py"
+echo "build started......"
 #jekyll build
+echo "finished jekyll build, start indexing......."
 python ./build_index.py
+echo "Indexing complete......."
+echo "Copy data file to _site"
 cp search.json _site/search.json
+echo "Syncing _site with s3: folder"
+aws s3 sync _site/ s3://fedramp-explorer/ 
+echo "Creating new cache invalidation template"
+
+CALLER_REF=$(gdate +%s)
+CALLER_JSON=$(sed -e 's/cref/'$CALLER_REF'/g' invalidate-cache.json.template)
+echo "invalidating cache with....."
+echo $CALLER_JSON
+echo $CALLER_JSON > invalidate-cache.json
+aws cloudfront create-invalidation  --cli-input-json file://invalidate-cache.json
